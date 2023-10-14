6565b130241c214b9332656d0ea74b9d7fcdd014
choi <chlxx0040639@gmail.com>
2023-10-14 18:29:47 +0900

    7주차

--- routes/books.js
+++ routes/books.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: '도서검색' ,pageName:'books/search.ejs'});
 });
-
+//장바구니 페이지로 이동
+router.get('/cart', function(req, res) { //next 생략 가능 
+  res.render('index', { title: '장바구니' ,pageName:'books/cart.ejs'});
+});
 module.exports = router;
