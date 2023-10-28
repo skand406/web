7aad83d8bcc88c9c3c4d1d4c794d2485dc05b13d
choi <chlxx0040639@gmail.com>
2023-10-28 17:49:14 +0900

    9주차

--- routes/locals.js
+++ routes/locals.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: '지역검색' ,pageName:'locals/search.ejs'});
 });
 
+router.get('/favorite', function(req, res, next) {
+  res.render('index', { title: '즐겨찾기' ,pageName:'locals/favorite.ejs'});
+});
+
 module.exports = router;
