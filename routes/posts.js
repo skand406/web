7f898a84223223144a6ea59c0da43f0eeaccaf6f
choi <chlxx0040639@gmail.com>
2023-11-18 18:24:28 +0900

    12주차

--- routes/posts.js
+++ routes/posts.js
@@ -10,5 +10,10 @@ router.get('/', function(req, res, next) {
 router.get('/insert', function(req, res, next) {
     res.render('index', { title: '글쓰기', pageName: 'posts/insert.ejs' });
 });
+/* 글읽기 페이지 */
+router.get('/read', function(req, res, next) {
+  const id=req.query.id;
+  res.render('index', { title: '게시글 정보', pageName: 'posts/read.ejs', id }); //id:id
+});
 
 module.exports = router;
